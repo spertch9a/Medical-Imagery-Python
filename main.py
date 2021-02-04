82073b87bf5b07f3684843f4b1168cf0c104fe31
Fortas Oussama Ilyes <fortasoussamailyes@gmail.com>
2021-02-05 00:23:17 +0100

    plotting the image and setting the contrast

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Starting off with imports
 # Hoping I can keep up with commenting and remembering every bit of code
 import imageio as image
+import matplotlib.pyplot as plt
+
 
 # Load "chest-220.dcm"
 im = image.imread("./data/tcia-chest-ct/chest-220.dcm")
@@ -18,3 +20,19 @@ print(im.meta)
 # printing the sex of the patient (accessing a metadata
 
 print("Patient sex is : " + im.meta['PatientSex'])
+
+
+# Draw the image in grayscale
+plt.imshow(im, cmap="gray")
+
+# Render the image
+plt.show()
+# //custom
+# Draw the image with greater contrast
+plt.imshow(im, cmap='gray', vmin=-200, vmax=200)
+
+# turn off the axis and ticks
+plt.axis('off')
+
+# Render the image
+plt.show()
